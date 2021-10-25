5bda2d6cfeda449e9bc93cc496d8ba96d31f930f
deansyue <c1318131801@gmail.com>
2021-10-25 22:49:39 +0800

    feat: add update todo function

--- app.js
+++ app.js
@@ -62,7 +62,29 @@ app.get('/todos/:id', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
     .lean()
-    .then(todo => res.render('detail', { todo }))
+    .then((todo) => res.render('detail', { todo }))
+    .catch(error => console.log(error))
+})
+
+//設定按下Edit後的路由
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+
+//設定使用save傳送出修改表單的路由
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
     .catch(error => console.log(error))
 })
 
