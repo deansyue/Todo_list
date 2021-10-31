9b3635126d9cd9b060497a38bc66cc5bba58eb48
deansyue <c1318131801@gmail.com>
2021-10-31 21:00:36 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   Todo.find() //取出todo model的所有資料
     .lean() //把Mongoose 的 Model物件轉換成乾淨的javaScript資料陣列
+    .sort({ _id: 'asc' }) //將資料依id排序
     .then(todos => res.render('index', { todos })) //將資料傳給index樣本
     .catch(error => console.error(error)) //錯誤處理
 
